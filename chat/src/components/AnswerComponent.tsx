62d2e428cca9ac2a7b0297d262eebfc25cf51cf6
ixxxl <106831329+ixxxl@users.noreply.github.com>
2023-04-27 14:58:57 +0300

    env1

--- chat/src/components/AnswerComponent.tsx
+++ chat/src/components/AnswerComponent.tsx
@@ -1,18 +1,22 @@
-const AnswerComponent = () => {
+const AnswerComponent = ({ storedValues }: any) => {
+  const copyText = (text: string) => {
+    navigator.clipboard.writeText(text);
+  };
   return (
     <>
       <hr className="hr-line" />
       <div className="answer-container">
-        <div className="answer-section">
-          <p className="question">Who is the founder of OpenAi?</p>
-          <p className="answer">
-            OpenAI was founded in December 2015 by Elon Musk, Sam Altman, Greg
-            Brockman, Ilya Sutskever, Wojciech Zaremba, and John Schulman.
-          </p>
-          <div className="copy-icon">
-            <i className="fa-solid fa-copy"></i>
-          </div>
-        </div>
+        {storedValues.map((value:any, index:number) => {
+          return (
+            <div className="answer-section" key={index}>
+              <p className="question">{value.question}</p>
+              <p className="answer">{value.answer}</p>
+              <div className="copy-icon" onClick={() => copyText(value.answer)}>
+                <i className="fa-solid fa-copy"></i>
+              </div>
+            </div>
+          );
+        })}
       </div>
     </>
   );
--- chat/src/components/FormComponent.tsx
+++ chat/src/components/FormComponent.tsx
@@ -1,14 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
-const FormComponent = () => {
+const FormComponent = ({ generateResponse }:any) => {
+  const [newQuestion, setNewQuestion] = useState<string>("");
+  const btnChangeHandler = (e:any) => {
+    generateResponse(newQuestion, setNewQuestion)
+  };
   return (
     <div className="form-section">
       <textarea
-       // rows="5"
+        rows={5}
         className="form-control"
         placeholder="Întreaba orice..."
+        value={newQuestion}
+        onChange={(e) => setNewQuestion(e.target.value)}
       ></textarea>
-      <button className="btn">Trimite</button>
+      <button className="btn" onClick={btnChangeHandler}>
+        Trimite
+      </button>
     </div>
   );
 };
